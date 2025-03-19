187027e1b42de20846d52ba38120515f534067f8
osaf1 <zsafi073@gmail.com>
2025-03-19 13:27:27 +0100

    voeg teller in while-lus.php voor tellen

---
+++ while-lus.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Document</title>
+</head>
+<body>
+<?php
+ error_reporting(E_ALL);
+ ini_set('display_errors', 1);
+
+ $teller = 1;
+ $maximum = 10;
+
+ echo "Teller is voor de while lus ".$teller."<br>";  
+
+ while ($teller <= $maximum) {
+    echo "Teller is nu ".$teller."<br>"; 
+    $teller++;
+ }
+
+ echo "Teller is na de while lus ".$teller."<br>";  
+?>
+
+</body>
+</html>
