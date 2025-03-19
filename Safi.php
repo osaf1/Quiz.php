63af0e26b9d5f33da36cd35fb532244a0013337f
osaf1 <zsafi073@gmail.com>
2025-03-19 09:54:09 +0100

    update text variable 'Hello, Safi1'

---
+++ Safi.php
@@ -0,0 +1,18 @@
+<?php
+
+//echo "Hello osafi, PHP CLI!\n";
+
+
+//$tekst = "Hello World!\n";
+//for ($i = 0; $i < 5; $i++) {echo $tekst;
+//}
+
+$tekst="Hello, Safi1\n";
+
+echo $tekst;
+echo $tekst;
+echo $tekst;
+echo $tekst;
+echo $tekst;
+
+?>
