ce4c29334d57d086fa26c8b99ed242818425bd14
osaf1 <zsafi073@gmail.com>
2025-03-19 09:07:55 +0100

    eerste commit HelloWorld.php toegevoegd

--- HelloWorld.php
+++ HelloWorld.php
@@ -1,6 +1,18 @@
 <?php
 
-echo "Hello osafi, PHP CLI!\n";
+//echo "Hello osafi, PHP CLI!\n";
+
+
+//$tekst = "Hello World!\n";
+//for ($i = 0; $i < 5; $i++) {echo $tekst;
+//}
+
+$tekst="Hello World!\n";
+
+echo $tekst;
+echo $tekst;
+echo $tekst;
+echo $tekst;
+echo $tekst;
 
 ?>
-
